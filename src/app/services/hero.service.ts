57e1dfb055b60cc6ae2ecacf3717bb174c839c3c
rhrynko-intellias <roman.hrynko@intellias.com>
2024-05-13 13:01:04 +0300

    create the service folder

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
-import { HEROES, Hero } from './hero';
+import { HEROES, Hero } from '../hero';
 
 @Injectable({
   providedIn: 'root',
